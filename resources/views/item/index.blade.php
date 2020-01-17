c720e0d284bfcdbe0f00b2e6640c1579d7ab1242
nanakahorii7 <t1dxoxo@yahoo.co.jp>
2020-01-17 14:18:06 +0900

    詳細ページの追加

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -6,7 +6,7 @@
             @foreach ($items as $item)
             <div class="col-md-4 mb-2">
                 <div class="card">
-                    <div class="card-header">{{ $item->name }}</div>
+                    <div class="card-header"><a href="/item/{{ $item->id }}">{{ $item->name }}</a></div>
                     <div class="card-body">
                         {{ $item->amount }}
                     </div>
@@ -15,7 +15,8 @@
             @endforeach
         </div>
         <div class="row justify-content-center">
-            {{ $items->links() }}
+        <!-- ページを移動しても入力データが引き継がれるようにする -->
+            {{ $items->appends(['keyword' => Request::get('keyword')])-> links() }}
         </div>
     </div>
 @endsection
