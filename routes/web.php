96835de66244093cce87f5476b0241b833b907ed
nanakahorii7 <t1dxoxo@yahoo.co.jp>
2020-01-20 13:36:28 +0900

    ECsite完成

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/cartitem', 'CartItemController@index');
 Route::post('/cartitem', 'CartItemController@store');
 Route::delete('/cartitem/{cartItem}', 'CartItemController@destroy');
 Route::put('/cartitem/{cartItem}', 'CartItemController@update');
+Route::get( '/buy', 'BuyController@index');
+Route::post( '/buy', 'BuyController@store');
+
 
 
 
